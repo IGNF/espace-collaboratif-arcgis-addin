2b36a5ea6db227816c68e59af2b943897624f6f8
Eric Peyrouse <eric.peyrouse@ign.fr>
2021-06-17 17:00:53 +0200

    Début fiche signalement : avec les fichiers c est mieux

---
+++ AddIn/ArcGisProEspaceCollaboratif/ArcGisProEspaceCollaboratif/ViewModels/SeeReportViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcGisProEspaceCollaboratif.ViewModels
+{
+    class SeeReportViewModel
+    {
+
+        #region Binding
+
+        public string NumberReport { get; set; } = "Signalement n°"
+
+        #endregion
+
+    }
+}
