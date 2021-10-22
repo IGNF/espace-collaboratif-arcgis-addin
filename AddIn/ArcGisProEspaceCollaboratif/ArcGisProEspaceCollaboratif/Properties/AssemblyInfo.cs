353a657d9bd6bb5f667f412384d26ff87f29a602
Eric Peyrouse <eric.peyrouse@ign.fr>
2021-10-22 16:02:17 +0200

    Mise à jour n° version suite livraison

--- AddIn/ArcGisProEspaceCollaboratif/ArcGisProEspaceCollaboratif/Properties/AssemblyInfo.cs
+++ AddIn/ArcGisProEspaceCollaboratif/ArcGisProEspaceCollaboratif/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("AddIn intégrant les fonctionnalités de signalement et d'écriture de l'Espace collaboratif")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IGN")]
-[assembly: AssemblyProduct("Test crash")]
+[assembly: AssemblyProduct("Toujours test crash")]
 [assembly: AssemblyCopyright("Copyright © IGN 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.3")]
+[assembly: AssemblyFileVersion("0.0.0.3")]
